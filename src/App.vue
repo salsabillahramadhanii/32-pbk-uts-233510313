7321efce7d915aec663b3c28f42ce1bdc7191663
unknown <salsabila02018@gmail.com>
2025-05-06 21:48:46 +0700

    User dapat menambahkan kegiatan baru

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>Catatan Aktivitas Harian</h1>
+    <div class="input-group">
+      <input v-model="newTodo" placeholder="Tambahkan kegiatan..." />
+      <button @click="addTodo">Tambah</button>
+    </div>
+
     <ul>
       <li v-for="(task, index) in tasks" :key="index">
         {{ task.name }}
@@ -16,4 +21,13 @@ const tasks = ref([
   { name: 'Memasak', completed: false },
   { name: 'Belajar', completed: false }
 ])
+const newTodo = ref('')
+
+const addTodo = () => {
+  if (newTodo.value.trim() !== '') {
+    tasks.value.push({ name: newTodo.value, completed: false })
+    newTodo.value = ''
+  }
+}
+
 </script>
