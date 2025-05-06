003ab4cb029ca7ac44a7b9005327f68dfc94ecb9
unknown <salsabila02018@gmail.com>
2025-05-06 21:21:37 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Daftar Kegiatan</h1>
+    <ul>
+      <li v-for="(task, index) in filteredTodos" :key="index">
+        {{ task.name }}
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+
+const tasks = ref([
+  { name: 'makan', completed: false },
+  { name: 'minum', completed: false }
+])
+</script>
